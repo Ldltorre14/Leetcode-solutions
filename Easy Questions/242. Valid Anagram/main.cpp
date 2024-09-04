584f6796220028a4857d165980c3e93c61f0bfa4
Leonardo Perez <83437994+Ldltorre14@users.noreply.github.com>
2024-09-03 19:43:42 -0600

    Update main.cpp

--- Easy Questions/242. Valid Anagram/main.cpp
+++ Easy Questions/242. Valid Anagram/main.cpp
@@ -17,7 +17,7 @@ Output: false
 //METHOD 1. SORTING
 //We know that a word is anagram to another, when they have the same length, letters and letter frequency (quantity of each letter)
 //Then if we sort both words, then both words should look like the same
-bool isAnagram(string s, string t) {
+bool isAnagram1(string s, string t) {
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         if (s == t)
@@ -26,5 +26,25 @@ bool isAnagram(string s, string t) {
 }
 
 
+//METHOD 2. Counting frequencies with a Hashmap
+//Basically, we count the frequency of each character in both strings, and we save them at their correspondent hashmap
+//If both hashmap are equal, then s is an anagram of t
+bool isAnagram2(string s, string t) {
+        if(s.length() != t.length())
+            return false;
+
+        map<char, int> count1;
+        map<char, int> count2;
+
+        for(int i=0; i<s.length(); i++){
+            count1[s[i]]++;
+            count2[t[i]]++;
+        } 
+
+        if(count1 != count2)
+            return false;
+
+        return true;
+    }
 
 
